ab2215b4991bdf771ef82c24dd6c4b35e018bbcc
l0u0h0 <l0u0h0@gmail.com>
2022-04-23 15:09:47 +0900

    tldrmfjdnswnakf

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from "./images/logo.jpg";
-import "./App.css";
+import "./css/App.css";
+import "./css/MainComponent.css";
 import MainComponent from "./components/MainComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>간장 공장 공장장 강공장장</p>
-        <MainComponent />
+        <div className="App-title">
+          <button>Dark</button>
+          <button>Light</button>
+        </div>
       </header>
+      <body>
+        <div className="App-body">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>간장 공장 공장장 강공장장</p>
+          <MainComponent />
+        </div>
+      </body>
     </div>
   );
 }
