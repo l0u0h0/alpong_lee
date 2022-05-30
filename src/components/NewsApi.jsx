080f261eb76178dcaa420ad9869ce8f8994f0792
l0u0h0 <l0u0h0@gmail.com>
2022-05-30 22:28:33 +0900

    pagination_fix

--- src/components/NewsApi.jsx
+++ src/components/NewsApi.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
 
 export default function NewsApi() {
   // NewsApi에서 받아온 값을 저장할 State, 초기값 null
