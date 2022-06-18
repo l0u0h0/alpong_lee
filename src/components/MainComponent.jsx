f863dbaf1fbde8b33d56dbb94fec83c0a3ebab13
l0u0h0 <l0u0h0@gmail.com>
2022-06-18 15:07:23 +0900

    cell_update

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -5,19 +5,29 @@ export default function MainComponent() {
     <div className="canvas">
       <div className="building">
         <div className="floor">
-          <div className="cell"></div>
+          <div className="cell">
+            <h1>01</h1>
+          </div>
         </div>
         <div className="floor">
-          <div className="cell"></div>
+          <div className="cell">
+            <h1>02</h1>
+          </div>
         </div>
         <div className="floor">
-          <div className="cell"></div>
+          <div className="cell">
+            <h1>03</h1>
+          </div>
         </div>
         <div className="floor">
-          <div className="cell"></div>
+          <div className="cell">
+            <h1>04</h1>
+          </div>
         </div>
         <div className="floor">
-          <div className="cell"></div>
+          <div className="cell">
+            <h1>05</h1>
+          </div>
         </div>
       </div>
     </div>
